fadcd80fd24f4e6004355d0875e760228f2577bb
Youssef Ahmed <sofy73290@gmail.com>
2026-09-09 02:34:46 +0300

    Update application files

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ufzroumxehomggrxsoin.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_p5XoEKQBWvIQ-OH0Zg7Ymw_Ed02JCok';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
